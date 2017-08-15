98e67c5fbd401a542e5aa33bf7b13f802bb20c77
Estudiante <Estudiante@10.203.1.168>
2017-08-15 15:40:58 -0500

    Realicé un cambio.

---
+++ JavaApplication20/src/javaapplication20/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class main {
+    public static void main(String[] args){
+        Cancion c1 = new Cancion();
+        Cancion c2 = new Cancion();
+        c1.titulo = "CANCION 1";
+        c2.titulo = "Cancion 2";
+        c1.sonar();
+        c2.sonar();
+    
+    } 
+}
